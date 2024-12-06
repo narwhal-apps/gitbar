6ff0ccc29af49fc80a38e9c900adc4e4b53f6633
friskopp <42931285+Friskopp@users.noreply.github.com>
2024-12-06 12:21:19 +0100

    fix: reduce complexity

--- src/components/login/Login.svelte
+++ src/components/login/Login.svelte
@@ -1,8 +1,5 @@
 <script lang="ts">
-  import * as Form from '$lib/components/ui/form/index.js';
-  import { zodClient } from 'sveltekit-superforms/adapters';
-  import { type Infer, superForm, type ValidationErrors } from 'sveltekit-superforms';
-  import { loginSchema, type LoginSchema } from './schema';
+  import { loginSchema } from './schema';
   import { open } from '@tauri-apps/plugin-shell';
   import { onDestroy, onMount } from 'svelte';
   import { getServerPort } from '$lib/app';
@@ -13,26 +10,19 @@
   import { cn } from '$lib/utils';
   import { Input } from '$lib/components/ui/input';
   import { getAuthContext } from '$lib/stores/contexts';
+  import Label from '$lib/components/ui/label/label.svelte';
 
   let ghCtx = getAuthContext();
 
   const defaultHost = 'github.com';
+
+  let formData = $state({ token: '', hostname: defaultHost });
+  let fieldErrors = $state({ token: '', hostname: '' });
   let loading = $state(false);
   let processing = $state(false);
+
   let port: number;
   let unlistenFn: () => void;
-
-  const form = superForm(
-    {
-      token: '',
-      hostname: defaultHost,
-    },
-    {
-      validators: zodClient(loginSchema),
-    }
-  );
-
-  let fieldErrors: ValidationErrors<Infer<LoginSchema>> = $state({});
 
   function handleToken() {
     open(createAuthURL(port));
@@ -68,129 +58,107 @@
     unlistenFn();
   });
 
-  const { form: formData } = form;
+  const handleSubmit = async () => {
+    const result = loginSchema.safeParse(formData);
 
-  const handleSubmit = async (e: SubmitEvent) => {
-    e.preventDefault();
-    const { errors, valid } = await form.validateForm();
-
-    if (valid) {
+    if (result.success) {
       loading = true;
       try {
-        await ghCtx.signIn(form.capture().data);
+        await ghCtx.signIn(result.data);
       } finally {
         loading = false;
       }
     } else {
-      fieldErrors = errors;
+      const { token, hostname } = result.error.format();
+      fieldErrors = { token: token?._errors.at(0) ?? '', hostname: hostname?._errors.at(-1) ?? '' };
     }
   };
 </script>
 
-<div class="m-8">
-  <form onsubmit={handleSubmit}>
-    <Form.Field {form} name="token">
-      <Form.Control>
-        {#snippet children({ props })}
-          <div class="m-0 flex flex-row justify-between">
-            <Form.Label class={cn(fieldErrors.token?.length && 'text-destructive')}>Token</Form.Label>
-            {#if fieldErrors.token}
-              <Form.Label class="text-destructive">{fieldErrors.token.at(-1)}</Form.Label>
-            {/if}
-          </div>
-          <Input
-            {...props}
-            class={cn(fieldErrors.token?.length && 'border-destructive')}
-            placeholder="The 40 characters token generated on GitHub"
-            bind:value={$formData.token}
-          />
-        {/snippet}
-      </Form.Control>
-      <Form.Description class="pb-1">
-        To generate a token, go to GitHub,
-        <button
-          class="cursor-pointer underline hover:text-gray-500 dark:hover:text-gray-300"
-          onclick={() =>
-            open(
-              'https://github.com/settings/tokens/new?description=gitbar&default_expires_at=none&scopes=repo,read:org'
-            )}
+<div class="flex flex-col gap-2 p-8">
+  <div class="flex h-4 flex-row items-center justify-between">
+    <Label for="token">Token</Label>
+    {#if fieldErrors.token}
+      <p class="text-sm text-red-400 dark:text-red-300">{fieldErrors.token}</p>
+    {/if}
+  </div>
+  <Input
+    class={cn(fieldErrors.token && 'border-red-300')}
+    placeholder="The 40 characters token generated on GitHub"
+    bind:value={formData.token}
+  />
+  <span class="text-sm">
+    To generate a token, go to GitHub,
+    <button
+      class="cursor-pointer underline hover:text-gray-500 dark:hover:text-gray-300"
+      onclick={() =>
+        open('https://github.com/settings/tokens/new?description=gitbar&default_expires_at=none&scopes=repo,read:org')}
+    >
+      personal access tokens
+    </button>
+  </span>
+  <div class="flex h-4 flex-row items-center justify-between">
+    <Label for="hostname">Hostname</Label>
+    {#if fieldErrors.hostname}
+      <p class="text-sm text-red-400 dark:text-red-300">{fieldErrors.hostname}</p>
+    {/if}
+  </div>
+  <Input
+    class={cn(fieldErrors.hostname && 'border-red-300')}
+    placeholder="github.company.com"
+    bind:value={formData.hostname}
+  />
+  <span class="text-sm">
+    Defaults to {defaultHost}. Change only if you are using GitHub for Enterprise.
+  </span>
+  <div class="flex flex-col items-center gap-1">
+    <Button
+      variant="default"
+      size="sm"
+      class={cn('w-full', loading && 'opacity-50')}
+      disabled={loading}
+      title="Submit"
+      onclick={handleSubmit}
+      >Submit {#if loading}
+        <svg
+          class="ml-3 h-4 w-4 animate-spin text-white"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
         >
-          personal access tokens
-        </button></Form.Description
-      >
-    </Form.Field>
-
-    <Form.Field {form} name="hostname">
-      <Form.Control>
-        {#snippet children({ props })}
-          <div class="m-0 flex flex-row justify-between">
-            <Form.Label class={cn(fieldErrors.hostname?.length && 'text-destructive')}>Hostname</Form.Label>
-            {#if fieldErrors.hostname}
-              <Form.Label class="text-destructive">{fieldErrors.hostname.at(-1)}</Form.Label>
-            {/if}
-          </div>
-          <Input
-            {...props}
-            placeholder="github.company.com"
-            bind:value={$formData.hostname}
-            class={cn(fieldErrors.hostname?.length && 'border-destructive')}
+          <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4" />
+          <path
+            class="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
           />
-        {/snippet}
-      </Form.Control>
-      <Form.Description class="pb-1">
-        Defaults to {defaultHost}. Change only if you are using GitHub for Enterprise.
-      </Form.Description>
-    </Form.Field>
-
-    <div class="flex flex-col items-center gap-1">
-      <Button
-        variant="default"
-        size="sm"
-        class={cn('w-full', loading && 'opacity-50')}
-        disabled={loading}
-        title="Submit"
-        type="submit"
-        >Submit {#if loading}
-          <svg
-            class="ml-3 h-4 w-4 animate-spin text-white"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4" />
-            <path
-              class="opacity-75"
-              fill="currentColor"
-              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-            />
-          </svg>
-        {/if}</Button
-      >
-      OR
-      <Button
-        variant="secondary"
-        size="sm"
-        class={cn('w-full', processing && 'opacity-50')}
-        disabled={processing}
-        onclick={handleToken}
-        title="Login via GitHub"
-        >Login via GitHub
-        {#if processing}
-          <svg
-            class="ml-3 h-4 w-4 animate-spin text-white"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4" />
-            <path
-              class="opacity-75"
-              fill="currentColor"
-              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-            />
-          </svg>
-        {/if}
-      </Button>
-    </div>
-  </form>
+        </svg>
+      {/if}</Button
+    >
+    OR
+    <Button
+      variant="secondary"
+      size="sm"
+      class={cn('w-full', processing && 'opacity-50')}
+      disabled={processing}
+      onclick={handleToken}
+      title="Login via GitHub"
+      >Login via GitHub
+      {#if processing}
+        <svg
+          class="ml-3 h-4 w-4 animate-spin text-white"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4" />
+          <path
+            class="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+          />
+        </svg>
+      {/if}
+    </Button>
+  </div>
 </div>
--- src/components/login/schema.ts
+++ src/components/login/schema.ts
@@ -3,14 +3,12 @@ import { z } from 'zod';
 export const loginSchema = z.object({
   token: z
     .string()
-    .min(40) // GitHub PATs are at least 40 characters
-    .max(255) // Setting a reasonable maximum length
+    .min(40, {message: 'Token must be at least 40 characters long'})
+    .max(255)
     .regex(/^ghp_[A-Za-z0-9]{36}$/, "Must be a valid token starting with 'ghp_'"),
   hostname: z
     .string()
-    .min(4) // Minimum length for a valid hostname
-    .max(255) // Maximum length for a hostname
+    .min(4)
+    .max(255)
     .regex(/^github\.(com|[a-zA-Z0-9-]+\.[a-zA-Z0-9-]+)$/, 'Must be a valid hostname'),
 });
-
-export type LoginSchema = typeof loginSchema;
